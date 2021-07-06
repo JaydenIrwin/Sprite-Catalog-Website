126a858fd276fa9231b182d2323161efe9146dd2
Jayden Irwin <me@jaydenirwin.com>
2021-07-05 21:09:54 -0400

    js

--- global/script.js
+++ global/script.js
@@ -5,11 +5,47 @@ function showSprite(title, id, artistName, licence) {
 
 	let artistLink = $("#artist-link");
 	artistLink.text(artistName);
-	artistLink.attr("href", "");
+	switch (licence) {
+		case "art":
+		artistLink.attr("href", "");
+		break;
+		case "art":
+		artistLink.attr("href", "");
+		break;
+		case "art":
+		artistLink.attr("href", "");
+		break;
+		case "art":
+		artistLink.attr("href", "");
+		break;
+		default:
+		artistLink.attr("href", "");
+		break;
+	}
 
 	let licenceLink = $("#licence-link");
-	licenceLink.text(licence);
-	licenceLink.attr("href", "");
+	switch (licence) {
+		case "CC0":
+		licenceLink.text("Public Domain");
+		licenceLink.attr("href", "https://creativecommons.org/publicdomain/zero/1.0/");
+		break;
+		case "CC BY":
+		licenceLink.text("Free with Attribution");
+		licenceLink.attr("href", "https://creativecommons.org/licenses/by/4.0");
+		break;
+		case "CC BY-SA":
+		licenceLink.text("Free with Attribution-Share Alike");
+		licenceLink.attr("href", "https://creativecommons.org/licenses/by-sa/4.0");
+		break;
+		case "CC BY-NC":
+		licenceLink.text("Free for Personal Use");
+		licenceLink.attr("href", "https://creativecommons.org/licenses/by-nc/4.0/");
+		break;
+		default:
+		licenceLink.text(licence);
+		licenceLink.attr("href", "");
+		break;
+	}
 
 	let downloadButton = $("#download");
 	downloadButton.attr("download", title);
